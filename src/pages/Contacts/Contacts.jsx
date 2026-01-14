47c33464f68687fe0c80349d99e78997bad6beca
Lautan Teguh <lautanteguh1@gmail.com>
2026-01-14 11:39:18 +0700

    Semi-encrypted emails for redirection with base64

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import './Contacts.css';
 
 function Contacts() {
+
+    const parts = ["bGF1dGFudGVndW", "gxQGdtYWlsLmNvbQ=="];
+    const email = atob(parts.join(""));
+
+
     return (
         <div className="contacts-page">
             <h1 className="contacts-heading">Contact Us</h1>
             <div className="contacts-info">
                 <p>If you have any questions or need assistance, feel free to reach out to us:</p>
-                <p><strong>Email:</strong> <a href={"mailto: contact@gmail.com"}>contact@example.com</a></p>
+                <p><strong>Email:</strong> <a href={`mailto:${email}`} id={"email"}>{email}</a></p>
                 <p><strong>Phone:</strong> (123) 456-7890</p>
                 <p><strong>Address:</strong> 123 Main Street, Anytown, USA</p>
                 <p><br/>We'd love to hear from you!</p>
